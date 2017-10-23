bd7a12fd53b382e6aeee64bf85c39d1044bc2569
IvanOpushkin <farm.vanya@yandex.ru>
2017-10-23 22:42:51 +1000

    hw0 commit 1st part

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -20,20 +20,58 @@ public class MatrixUtil {
 
     // TODO optimize by https://habrahabr.ru/post/114797/
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
+
+        //1.п уже сделан. размер файнал *п52 выключен Java типо* -простой случай квадратной таблицы
         final int matrixSize = matrixA.length;
+
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize; j++) {
-                int sum = 0;
-                for (int k = 0; k < matrixSize; k++) {
-                    sum += matrixA[i][k] * matrixB[k][j];
-                }
-                matrixC[i][j] = sum;
+        //2.п переносим столбцы в строки второй таблицы
+/*
+        int[][] optimize = new int[matrixSize][matrixSize];
+
+        for (int i = 0; i < matrixSize; i++)
+            for (int j = 0; j < matrixSize; j++)
+            {
+                optimize[i][j] = matrixB[j][i];
             }
+
+*/
+
+//п19 вынос за скобку общего и перемножение банально без вынесенго за скобку AB-DA=(B-D)*A ПРОФЕССОР LOGIC
+
+
+        try {
+            for (int i = 0; i < matrixSize; i++) {
+
+                int[] bRow = new int[matrixSize];
+
+                for (int k = 0; k < matrixSize; k++)
+                    bRow[k] = matrixB[k][i]; //переворот столбца в строку (automatic)
+
+
+                for (int j = 0; j < matrixSize; j++) {
+                    int sum = 0;
+                    int[] rowA = matrixA[j];//строки в А нас устраивают изначально
+                    for (int k = 0; k < matrixSize; k++) {
+                        sum += bRow[k] * rowA[k];
+
+
+                        //заменили матрикс б на нашу перевернутую
+                        // sum += matrixA[i][k] * matrixB[k][j];
+                        // sum += matrixA[i][k] * optimize[j][k];
+
+                    }
+                    matrixC[i][j] = sum;
+                }
+            }
+        }catch(IndexOutOfBoundsException ignore)
+        {
+            //На случай выхода за массив = выгодно.
         }
-        return matrixC;
-    }
+            return matrixC;
+        }
+
 
     public static int[][] create(int size) {
         int[][] matrix = new int[size][size];
